5667b9ca0818a1f5dbf62cfeaf61993d368587ef
John Maybee <john.d.maybee@gmail.com>
2023-05-18 21:19:51 -0400

    Corrected an error that was caused by incorrect position of a v-else statement

--- arrested-development-forum/src/App.vue
+++ arrested-development-forum/src/App.vue
@@ -1,47 +1,104 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+      counterTitle: 'Counter Standard',
+      incrementAmount: 3,
+      message: 'Hello it works',
+      listOfNumbers: [
+        {
+          name: 1,
+          id: '6a887cd2-f0bf-4321-b192-92016f82a883',
+          list: [1, 2, 3]
+        },
+        {
+          name: 2,
+          id: '8d14d90b-2d47-473e-8293-d5c324111d0d',
+          list: [1, 2, 3]
+        },
+        {
+          name: 3,
+          id: 'cd806d65-2309-4625-9104-dcd636cd79b5',
+          list: [1, 2, 3]
+        },
+        {
+          name: 4,
+          id: '00c939cd-fbf4-46d0-8e61-0bc2ce8a5332',
+          list: [1, 2, 3]
+        },
+        {
+          name: 5,
+          id: '295a8170-59c1-4462-9de1-3c9cd41cedab',
+          list: [1, 2, 3]
+        }
+      ]
+    }
+  },
+  computed: {
+    displayTitle() {
+      if (this.count > 20) {
+        return 'Counter Standard - Very Long'
+      } else {
+        return 'Counter Standard'
+      }
+    },
+    optimizedIncrementAmount() {
+      return this.displayTitle.length * this.incrementAmount
+    }
+  },
+  methods: {
+    // changeIncrementAmount(event){
+    //   this.incrementAmount = event.target.value
+    // },
+    incrementCount(newAmount, event) {
+      console.log(newAmount)
+      console.log(event)
+      this.count += this.optimizedIncrementAmount 
+    }
+  },
+  watch: {
+    count(newValue) {
+      // console.log(newValue)
+      if (newValue > 20) {
+        this.counterTitle += ' Very Long'
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h2>{{ message }}</h2>
+  <h3>Statistics:</h3>
+  <ul>
+    <li v-for="(stat, type) in familyStatistics" :key="`character-${stat}-${type}`">{{ type }}: {{ stat }}</li>
+  </ul>
+  <ul>
+    {{ familyStatistics }}
+  </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h3>Characters:</h3>
+  <p v-if="characterList.length === 0">There are no characters</p>
+  <ul v-else-if="characterList.length % 2 === 0">
+    <li v-for="(character, index) in characterList" :key="`even-character-${index}`">
+      <p>{{ character.name }}</p>
+      <button @click="favoriteCharacter(character)">🔥 Favorite</button>
+    </li>
+  </ul>
+  <p v-else>There are an odd number of characters</p>
+  <h3>Favorite Characters</h3>
+  <ul v-if="favoriteList.length > 0">
+    <li v-for="(character, index) in favoriteList" :key="`odd-character-${index}`">
+      {{ character.name }}
+    </li>
+  </ul>
+  <p v-else>There are no favorite characters!</p>
+  <h3>New Character</h3>
+  <pre>
+    {{ newCharacter }}
+  </pre>
+  <label for="character-name">Name</label>
+  <input type="text" v-model="newCharacter.name"
+  @keyup.enter="addNewCharacter" />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
